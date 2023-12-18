e3ca5e3445f4dc329ab124d1463c5051660ffe80
waisiukei <wakiki@outlook.com>
2023-12-18 23:14:14 +0800

    wip

--- src/binding.cpp
+++ src/binding.cpp
@@ -1,4 +1,11 @@
+#include <emscripten.h>
+#include <stdio.h>
+
+
+#include <emscripten.h>
+#include <emscripten/console.h>
 #include <emscripten/bind.h>
+
 using namespace emscripten;
 
 class MyClass {
@@ -16,32 +23,8 @@ int getDataFromAddress(uintptr_t address) {
 }
 
 MyClass* getInstanceFromAddress(uintptr_t address) {
+    printf("123\n");
     return reinterpret_cast<MyClass*>(address);
-}
-
-void allocate(uintptr_t address) {
-    int i;
-    int len = 10;
-    char* p = reinterpret_cast<char *>(address);
-    for(i = 0; i < len; i++) {
-        p[i] = 'a' + (char)i;
-    }
-    p[len] = '\n';
-}
-char read(uintptr_t address) {
-    char* buffer = reinterpret_cast<char *>(address);
-    return buffer[0];
-}
-
-int getSize() {
-    return sizeof(char);
-}
-
-void modify(uintptr_t address, int length) {
-    uint8_t* data = reinterpret_cast<uint8_t *>(address);
-    for (int i = 0; i < length; i++) {
-        data[i] *= 2;  // 将每个元素乘以2
-    }
 }
 
 
@@ -53,8 +36,4 @@ EMSCRIPTEN_BINDINGS(my_class_example) {
   function("getAddress", &getAddress);
   function("getDataFromAddress", &getDataFromAddress);
   function("getInstanceFromAddress", &getInstanceFromAddress, allow_raw_pointers());
-  function("allocate", &allocate, allow_raw_pointers());
-  function("read", &read);
-  function("getSize", &getSize);
-  function("modify", &modify);
 }
--- src/main.ts
+++ src/main.ts
@@ -6,65 +6,33 @@ import wasmURL from './binding.wasm?url';
 import Worker from './worker?worker';
 
 async function main() {
-
-    var TOTAL_MEMORY = 16777216;
-    var WASM_PAGE_SIZE = 65536;
-    var wasmMemory = new WebAssembly.Memory({
-        'initial': TOTAL_MEMORY / WASM_PAGE_SIZE,
-        'maximum': TOTAL_MEMORY / WASM_PAGE_SIZE,
-        shared: true,
-    });
-    var buffer = wasmMemory.buffer;
-    let wasm: any;
+    let wasmModule;
     const Module = {
         async instantiateWasm(info: any, receiveInstance: any) {
             var importObject = Object.assign({}, info);
-            importObject['env']['memory'] = wasmMemory;
             const output = await WebAssembly.instantiateStreaming(fetch(wasmURL, { credentials: 'same-origin' }), importObject);
-            wasm = output;
-            return receiveInstance(output['instance']);
+            wasmModule = output.module;
+            const instance = output.instance;
+            return receiveInstance(instance, wasmModule);
         },
-        TOTAL_MEMORY,
-        buffer,
     } as any;
     const module = await moduleFactory(Module);
-    {
-        // 创建一个包含数据的Uint8Array
-        const uint8Array = new Uint8Array([1, 2, 3, 4, 5]);
-
-        // 获取WebAssembly线性内存的引用
-        const wasmMemory = module.HEAPU8.buffer;
-
-        // 创建一个内存视图来读写WebAssembly线性内存中的数据
-        const memoryView = new Uint8Array(wasmMemory);
-
-        // 将Uint8Array的数据直接复制到内存视图
-        memoryView.set(uint8Array);
-
-        // 调用WebAssembly模块中的函数来修改数据
-        module.modify(memoryView.byteOffset, uint8Array.length);
-
-        // 从内存视图中读取修改后的数据
-        // var modifiedData = new Uint8Array(wasmMemory, memoryView.byteOffset, uint8Array.length);
-    }
-
     const worker = new Worker();
     worker.postMessage({
         method: 'init',
-        wasm: wasm.instance.exports,
+        wasmModule: wasmModule,
+        wasmMemory: module.wasmMemory,
     });
 
     worker.onmessage = (e) => {
         if (e.data.inited) {
-
-            console.log(module.getSize());
-            // var ptr = module._malloc(1 * 10);
-            //
-            // module.allocate(ptr);
-            // console.log('main', module.read(ptr), module.HEAP8[ptr]);
+            const obj = new module.MyClass();
+            obj.data = 123;
+            const ptr = module.getAddress(obj);
 
             worker.postMessage({
                 method: 'call',
+                ptr,
             });
         }
     };
